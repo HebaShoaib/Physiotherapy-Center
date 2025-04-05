558c7d7c53bebeb160bd63c4c35a08ccddef920b
Mohamed Khaled <mohamed191306@gmail.com>
2025-04-05 17:27:00 +0200

    Stack implementation is complete

--- Stack.h
+++ Stack.h
@@ -1,5 +1,48 @@
 #pragma once
+#include <iostream>
+#include "Node.h"
+using namespace std;
 class Stack
 {
+public:
+    Node* top;
+    
+    bool isEmpty() {
+        if (top == NULL)
+            return true;
+        else
+            return false;
+    }
+    
+    bool push(int data) {
+        Node* newNode = new Node;
+        newNode->data = data;
+        if (isEmpty()) {
+            top=newNode;
+            newNode->next = NULL;
+            return true;
+        }
+    newNode->next = top;
+        top = newNode;
+
+        return true;
+        }
+        
+    bool Pop() {
+        if (isEmpty()) {
+            return false;
+        }
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+        return true;
+    }
+
+    int peek() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return top->data;
+    }
 };
 
